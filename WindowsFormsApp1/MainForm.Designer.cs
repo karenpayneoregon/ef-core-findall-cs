bff4cc7f7e600229305089e1ac51d86c6e968408
Karen Payne <paynekaren@comcast.net>
2020-04-30 13:58:45 -0700

    Removed unused

--- WindowsFormsApp1/MainForm.Designer.cs
+++ WindowsFormsApp1/MainForm.Designer.cs
@@ -31,11 +31,13 @@
             this.button1 = new System.Windows.Forms.Button();
             this.CategoryCheckedListBox = new System.Windows.Forms.CheckedListBox();
             this.ResultsTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(198, 12);
+            this.button1.Location = new System.Drawing.Point(198, 20);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(91, 23);
             this.button1.TabIndex = 1;
@@ -46,7 +48,7 @@
             // CategoryCheckedListBox
             // 
             this.CategoryCheckedListBox.FormattingEnabled = true;
-            this.CategoryCheckedListBox.Location = new System.Drawing.Point(12, 12);
+            this.CategoryCheckedListBox.Location = new System.Drawing.Point(12, 20);
             this.CategoryCheckedListBox.Name = "CategoryCheckedListBox";
             this.CategoryCheckedListBox.Size = new System.Drawing.Size(168, 184);
             this.CategoryCheckedListBox.TabIndex = 2;
@@ -54,18 +56,38 @@
             // ResultsTextBox
             // 
             this.ResultsTextBox.Font = new System.Drawing.Font("Courier New", 9.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.ResultsTextBox.Location = new System.Drawing.Point(309, 12);
+            this.ResultsTextBox.Location = new System.Drawing.Point(309, 20);
             this.ResultsTextBox.Multiline = true;
             this.ResultsTextBox.Name = "ResultsTextBox";
             this.ResultsTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
             this.ResultsTextBox.Size = new System.Drawing.Size(331, 184);
             this.ResultsTextBox.TabIndex = 3;
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 2);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Categories";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(313, 5);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "FindAll";
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(649, 216);
+            this.ClientSize = new System.Drawing.Size(649, 238);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.ResultsTextBox);
             this.Controls.Add(this.CategoryCheckedListBox);
             this.Controls.Add(this.button1);
@@ -82,5 +104,7 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.CheckedListBox CategoryCheckedListBox;
         private System.Windows.Forms.TextBox ResultsTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
